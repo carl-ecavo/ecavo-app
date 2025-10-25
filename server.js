252ef4f1a9191c51e9779af2a776878244b91103
Carl Cronstedt <carl@ecavo.se>
2025-10-25 22:03:22 +0000

    CI/CD test: change message

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import http from "node:http";
 const server = http.createServer((req, res) => {
   res.writeHead(200, {"content-type": "text/plain"});
-  res.end("Hello from Ecavo!\n");
+  res.end("Hello from Ecavo! CI/CD!\n");
 });
 server.listen(process.env.PORT || 8080);
