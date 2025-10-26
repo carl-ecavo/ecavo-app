67f0ceaa64793e4c256471146372b5a3d9553bfe
carl-ecavo <carl@ecavo.se>
2025-10-26 14:05:46 +0000

    Fix: Explicitly bind server to 0.0.0.0 for Cloud Run

--- server.js
+++ server.js
@@ -42,6 +42,6 @@ const server = http.createServer((req, res) => {
     });
 });
 
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+server.listen(PORT, '0.0.0.0', () => { // <--- ADDED '0.0.0.0'
+  console.log(`Server listening on port ${PORT} at 0.0.0.0`);
 });
